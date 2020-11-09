2342118adc8c31c75d83be71be627ca2c38a0430
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2020-11-09 17:56:46 -0500

    Update updater.py

--- updater.py
+++ updater.py
@@ -2,7 +2,7 @@
 import urllib.request as dlurl
 from os import path
 from json import loads
-import zipfile
+from zipfile import ZipFile
 
 # Get latest released version
 try: resp = dlurl.urlopen('https://api.github.com/repos/Vianpyro/NSI-saints-days-game-project/releases/latest')
@@ -10,26 +10,27 @@ except:
     print('Unable to connect to web page, please check your internet connection.')
     quit()
 
-resp = dlurl.urlopen('https://api.github.com/repos/Vianpyro/NSI-saints-days-game-project/releases/latest')
 data = loads(resp.read())
 version = data['html_url'].split('/')[-1]
 
 # Download the latest version
 if path.exists(f'{version}.zip'):
-    print(f'Your game is running on the latest release ({version[1:]})!')
+    print(f'Your game is running on the latest release ({version})!')
 else:
     if __name__ == "__main__":
+        print(f'Downloading {version}...')
         dlurl.urlretrieve(
             f'https://github.com/Vianpyro/NSI-saints-days-game-project/archive/{version}.zip', 
             f'{version}.zip'
         )
-        print(f'Version {version[1:]} successfully downloaded.')
+        print(f'Downloaded {version} successfully.')
 
         if not path.exists(f'NSI-saints-days-game-project-{version[1:]}'):
             print(f'Extracting {version}...')
-            with zipfile.ZipFile(f'{version}.zip', 'r') as zipf:
+            with ZipFile(f'{version}.zip', 'r') as zipf:
                 zipf.extractall()
                 zipf.close()
+                print(f'Extracted {version} successfully.')
     else:
-        print(f"Une nouvelle version est peut être disponible : {version[1:]}, utilisez l'updater pour télécharger la dernière version!")
-        print(f'A new version may be available: {version[1:]}, use the updater to download the latest version!')
+        print(f"Une nouvelle version est peut être disponible : {version}, utilisez l'updater pour télécharger la dernière version!")
+        print(f'A new version may be available: {version}, use the updater to download the latest version!')
