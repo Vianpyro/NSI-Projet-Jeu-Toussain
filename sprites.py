824c30db2b0095fbd8a02a14a355b78f6fd48cf1
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2020-11-12 11:37:30 -0500

    Added animations

--- sprites.py
+++ sprites.py
@@ -2,11 +2,24 @@
 # -*- coding: utf8 -*-
 import pygame as pg
 from settings import *
+from xml.dom import minidom
+from os import path
 
 if __name__ == "__main__":
     quit()
  
-vec = pg.math.Vector2                               # Création d'une variable de type "vecteur en deux dimensions"
+vec = pg.math.Vector2                                       # Création d'une variable de type "vecteur en deux dimensions"
+doc = minidom.parse(path.join('images', 'spritesheet.xml')) # Création d'une "instance" de mon fichier xml
+items = doc.getElementsByTagName('SubTexture')              # Création d'un objet contenant les informations du fichier xml
+images = {}
+for i in range(len(items)):
+    images[items[i].attributes['name'].value.split('.')[0]] = {
+        'name': items[i].attributes['name'].value,
+        'x': int(items[i].attributes['x'].value),
+        'y': int(items[i].attributes['y'].value),
+        'width': int(items[i].attributes['width'].value),
+        'height': int(items[i].attributes['height'].value)
+    }
 
 class Spritesheet:                                  # Création de la classe Spritesheet qui charge ce(s) dernier(s)
     def __init__(self, filename):
@@ -22,24 +35,109 @@ class Player(pg.sprite.Sprite):                     # Création de la classe jou
     def __init__(self, game):
         pg.sprite.Sprite.__init__(self)                     # Initialisation de la classe superieur "sprite"
         self.game = game                                    # Sauvegarde du jeu aquel "appartient" le joueur
+        self.load_images()
         self.walking = False
         self.jumping = False
         self.current_frame = 0
         self.last_update = 0
-        self.image = self.game.spritesheet.get_sub_image(   # Définition de l'apparence de mon objet (joueur)
-            x=60, y=0, width=16, height=27
-        )
-        self.image = pg.transform.scale(                    # Agrandissement de l'image
-            self.image, (40, 64)
-        )
-        self.image.set_colorkey(BLACK)                      # Supression du "font" noir
+        self.image = self.idle_frame
         self.rect = self.image.get_rect()                   # Sauvegarde de la surface occupée par mon objet
         self.rect.center = (WIDTH / 2, HEIGHT / 2)          # Centrage du joueur dans la fenêtre
         self.position = vec(WIDTH / 2, HEIGHT / 2)          # Centrage de la position du joueur dans la fenêtre
         self.velocity = vec(0, 0)                           # Définition de la vélocité du joueur a 0x 0y
         self.acceleration = vec(0, 0)                       # Définition de l'accélération du joueur a 0x 0y
 
+    def animate(self):
+        now = pg.time.get_ticks()
+        if self.velocity.x != 0: self.walking = True
+        else: self.walking = False
+
+        if self.velocity.y < 0: self.jumping = True
+        else: self.jumping = False
+
+        if not self.walking and not self.jumping:
+            self.image = self.idle_frame
+
+
+        if self.walking:
+            if self.jumping:
+                if self.velocity.x < 0:
+                    self.image = self.jumping_left_frame
+                else:
+                    self.image = self.jumping_right_frame
+            else:
+                if now - self.last_update > 180:
+                    self.last_update = now
+                    self.current_frame = (self.current_frame + 1) % len(self.walking_left_frames)
+
+                    if self.velocity.x < 0:
+                        self.image = self.walking_left_frames[self.current_frame]
+                    else:
+                        self.image = self.walking_right_frames[self.current_frame]
+
+                    self.rect = self.image.get_rect()
+
+    def load_images(self):
+        self.idle_frame = pg.transform.scale(
+            self.game.spritesheet.get_sub_image(            # Définition de l'apparence de mon objet (joueur)
+                images['front']['x'],
+                images['front']['y'],
+                images['front']['width'],
+                images['front']['height']
+            ), (PLAYER_WIDTH, PLAYER_HEIGHT)
+        )
+
+        self.jumping_right_frame = pg.transform.scale(
+            self.game.spritesheet.get_sub_image(
+                images['jump_right']['x'],
+                images['jump_right']['y'],
+                images['jump_right']['width'],
+                images['jump_right']['height']
+            ), (PLAYER_WIDTH, PLAYER_HEIGHT)
+        )
+
+        self.jumping_left_frame = pg.transform.scale(
+            self.game.spritesheet.get_sub_image(
+                images['jump_left']['x'],
+                images['jump_left']['y'],
+                images['jump_left']['width'],
+                images['jump_left']['height']
+            ), (PLAYER_WIDTH, PLAYER_HEIGHT)
+        )
+
+        self.walking_right_frames = [
+            pg.transform.scale(
+                self.game.spritesheet.get_sub_image(
+                    images[f'walk_right_{i + 1}']['x'],
+                    images[f'walk_right_{i + 1}']['y'],
+                    images[f'walk_right_{i + 1}']['width'],
+                    images[f'walk_right_{i + 1}']['height']
+                ), (PLAYER_WIDTH, PLAYER_HEIGHT)
+            )
+            for i in range(2)
+        ]
+
+        self.walking_left_frames = [
+            pg.transform.scale(
+                self.game.spritesheet.get_sub_image(
+                    images[f'walk_left_{i + 1}']['x'],
+                    images[f'walk_left_{i + 1}']['y'],
+                    images[f'walk_left_{i + 1}']['width'],
+                    images[f'walk_left_{i + 1}']['height']
+                ), (PLAYER_WIDTH, PLAYER_HEIGHT)
+            )
+            for i in range(2)
+        ]
+
+        # Supression du "font" noir
+        self.idle_frame.set_colorkey(BLACK)
+        self.jumping_right_frame.set_colorkey(BLACK)
+        self.jumping_left_frame.set_colorkey(BLACK)
+        for a in self.walking_left_frames: a.set_colorkey(BLACK)
+        for b in self.walking_right_frames: b.set_colorkey(BLACK)
+
     def update(self):
+        self.animate()
         self.acceleration = vec(0, GRAVITY)                              # A chaque update (frame) on remet l'acceleration laterale a 0 et on ajoute la gravite
         if self.position.y >= HEIGHT + PLATFORM_HEIGHT:                  # Test pour voir si le joueur est tombé des platformes
             self.game.playing = False                                    # Arrêt de la partie
@@ -65,6 +163,7 @@ class Player(pg.sprite.Sprite):                     # Création de la classe jou
         self.acceleration.x += self.velocity.x * PLAYER_FRICTION    # Application de la friction au mouvement lateral ; plus on va vite plus on est freiné
         self.velocity += self.acceleration                          # Mouvement de l'objet selon son accélération
         self.position += self.velocity + 0.5 * self.acceleration    # Déplacement de l'objet selon l'équation du mouvement
+        if abs(self.velocity.x) < 0.1: self.velocity.x = 0
 
         # On fait en sorte que le joueur reste toujours dans la fenêtre de jeu
         if self.position.x < 0:
@@ -81,7 +180,7 @@ class Platform(pg.sprite.Sprite):
         self.image = self.game.spritesheet.get_sub_image(   # Définition de l'apparence de mon objet (Platform)
             0, 58, 195, 45
         )
-        self.image = pg.transform.scale(                    # Agrandissement de l'image
+        self.image = pg.transform.scale(    
             self.image, (width, height)
         )
         self.image.set_colorkey(BLACK)
@@ -103,7 +202,7 @@ class Cloud(pg.sprite.Sprite):
         self.image = self.game.spritesheet.get_sub_image(   # Définition de l'apparence de mon objet (Platform)
             0, 58, 195, 45
         )
-        self.image = pg.transform.scale(                    # Agrandissement de l'image
+        self.image = pg.transform.scale(    
             self.image, (width, height)
         )
         self.image.set_colorkey(BLACK)
--- updater.py
+++ updater.py
@@ -1,36 +1,76 @@
 # Import included libraries
 import urllib.request as dlurl
-from os import path
+from os import path, remove
 from json import loads
 from zipfile import ZipFile
 
-# Get latest released version
-try: resp = dlurl.urlopen('https://api.github.com/repos/Vianpyro/NSI-saints-days-game-project/releases/latest')
-except:
-    print('Unable to connect to web page, please check your internet connection.')
-    quit()
+# Parameters
+auto_update = True
+auto_download = True
+github_user = 'Vianpyro'
+github_repo = 'NSI-saints-days-game-project'
 
-data = loads(resp.read())
-version = data['html_url'].split('/')[-1]
+source_website = 'https://api.github.com'
 
-# Download the latest version
-if path.exists(f'{version}.zip'):
-    print(f'Your game is running on the latest release ({version})!')
-else:
-    if __name__ == "__main__":
-        print(f'Downloading {version}...')
-        dlurl.urlretrieve(
-            f'https://github.com/Vianpyro/NSI-saints-days-game-project/archive/{version}.zip', 
-            f'{version}.zip'
-        )
-        print(f'Downloaded {version} successfully.')
+# Updater parameters (Don't change this if you want to be aware of published updates)
+updater_auto_update = True
+updater_auto_download = True
+updater_user = 'Vianpyro'
+updater_repo = 'Python-Updater'
 
-        if not path.exists(f'NSI-saints-days-game-project-{version[1:]}'):
-            print(f'Extracting {version}...')
-            with ZipFile(f'{version}.zip', 'r') as zipf:
-                zipf.extractall()
-                zipf.close()
-                print(f'Extracted {version} successfully.')
-    else:
-        print(f"Une nouvelle version est peut être disponible : {version}, utilisez l'updater pour télécharger la dernière version!")
-        print(f'A new version may be available: {version}, use the updater to download the latest version!')
+# Creation of a list containing the variables in order to be able to process "easily" the link(s) to be processed
+parameters = [
+    [auto_update, auto_download, github_user, github_repo],
+    [updater_auto_update, updater_auto_download, updater_user, updater_repo]
+]
+
+for i in range(len(parameters)):
+    # Searching for the latest version if the program is set to auto update
+    if parameters[i][0]:
+        try:
+            resp = dlurl.urlopen(
+                f'{source_website}/repos/{parameters[i][2]}/{parameters[i][3]}/releases/latest'
+            )
+            data = loads(resp.read())
+            name = data['name']
+            version = data['html_url'].split('/')[-1]
+
+            if parameters[i][1]:
+                if path.exists(f'{name}.zip') or path.exists(f'{parameters[i][3]}-{version}') or path.exists(f'{parameters[i][3]}-{version[1:]}'):
+                    print(f'{parameters[i][2]}/{parameters[i][3]} is already running on the most recent version: {name}!')
+                elif __name__ == "__main__":
+                    try:
+                        print(f'Downloading {parameters[i][3]} {name}...')
+                        dlurl.urlretrieve(
+                            f'https://github.com/{parameters[i][2]}/{parameters[i][3]}/archive/{version}.zip',
+                            f'{parameters[i][3]} {name}.zip'
+                        )
+                        print(f'Downloaded {parameters[i][3]} {name} successfully.')
+                    except:
+                        print(
+                            'Could not download',
+                            f'{source_website}/{parameters[i][2]}/{parameters[i][3]}/archive/{version}.zip'
+                        )
+                    
+                    try:
+                        if not path.exists(f'{github_repo}-{version}'):
+                            print(f'Extracting {parameters[i][3]} {name}...')
+                            with ZipFile(f'{parameters[i][3]} {name}.zip', 'r') as zipf:
+                                zipf.extractall()
+                                zipf.close()
+                                remove(f'{parameters[i][3]} {name}.zip')
+                                print(f'Extracted {parameters[i][3]} {name} successfully.')
+                    except:
+                        print(
+                            f'Could not extract {name}.'
+                        )
+            else:
+                print(
+                    f'A new version of {parameters[i][2]}/{parameters[i][3]} may be available: {name}.'
+                )
+        except: 
+            print(
+                f'Unable to locate package: "{source_website}/repos/{parameters[i][2]}/{parameters[i][3]}/releases/latest"...'
+            )
+    if updater_user == github_user and updater_repo == github_repo and __name__ == "__main__":
+        quit()
